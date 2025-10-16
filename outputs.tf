7c7c76239d56821cacbce12df2fc8b0fa337a6a6
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-10-16 11:04:36 +0530

    .lk

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cdn_url" {
-  value = azurerm_cdn_endpoint.cdn_endpoint.endpoint_hostname
-}
-
-output "website_url" {
-  value = azurerm_storage_account_static_website.static_site.primary_web_endpoint
-}
